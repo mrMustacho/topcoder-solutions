b8f6b46f45b687cbcbd34a45e13206d46dc08a50
mrMustacho <almardones@alumnos.uai.cl>
2020-09-10 21:01:16 -0300

    Modified SoccerLeagues: Added Test cases

--- py/SoccerLeagues.py
+++ py/SoccerLeagues.py
@@ -4,6 +4,40 @@ Created on Mon Mar 25 15:01:38 2019
 
 @author: Alejandro
 """
+test1 = ["-WW",
+        "W-W",
+        "WW-"]
+
+test2 = ["-DD",
+        "L-L",
+        "WD-"]
+
+test3 = ["-DWWD",
+        "L-WLL",
+        "DD-WD",
+        "DDL-L",
+        "DDLL-"]
+
+test4 = ["-LWWLWDLDWWWWWWDDWDW",
+        "D-WWLDDWDWDLWDDWLWDD",
+        "LL-DLDWDLDLDWWWLWDDW",
+        "LDD-LLLDLWLWWWWDWDWL",
+        "LWWW-DWDLWDWDWWWDWDW",
+        "DLLWD-WWLLDDDLWWDWWW",
+        "WWLWDL-LLDWWWWWDWWLW",
+        "LLLLLDW-LDLWDDLLLDWL",
+        "DWWWWDDD-DWWWWDWWWDW",
+        "WWWWLLLWL-LWWWWWLWWW",
+        "DWWWWWWWLW-WDWWWWWWW",
+        "DDDLLLDWWWL-DDWDWLDD",
+        "LWLWLDLLLDLW-DDDWWDD",
+        "LLWWLWDDLWLWL-WWWDLL",
+        "WWWWLLDDDWLWDD-WWWLW",
+        "DLDLLLWWLLLWWLW-DWLL",
+        "DLWWWLDLWWDWWDWL-WWD",
+        "LLDDLLWLLWLWLDLWW-WW",
+        "LLWLLLWWLWLWWDWWLD-W",
+        "LLWDLWDWDWLLWWDDWWL-"]
 
 def points(matches):
     n = len(matches)
@@ -19,15 +53,32 @@ def points(matches):
                 plist[visit] += 3
     return plist
 
-print("Enter list of matches(press enter again to exit):")
+'''
+## Prompt
+
+print("Enter list of matches([W]in, [L], [D]raw or press Enter exit):")
 matches = []
 while True:
     buffer = input()
     if buffer == "":
         break
     matches.append(buffer)
+'''
 
+
+
+print(points(test1))
+# Returns: {6, 6, 6 }
+print(points(test2))
+# Returns: {5, 2, 8 }
+print(points(test3))
+# Returns: {14, 7, 12, 8, 10 }
+print(points(test4))
+# Returns: {72, 62, 41, 41, 83, 63, 53, 35, 86, 50, 90, 32, 34, 41, 45, 36, 51, 32, 51, 45 }
+
+'''
 print("Matches: ")
 print(matches)
 print("Scores: ")
 print(points(matches))
+'''
